cd9ccc41c773baa05726a32beab1327c907371a6
QuinntyneBrown <quinntynebrown@gmail.com>
2021-08-06 07:59:37 -0400

    feat(core): serve

--- src/Lit.Core/Features/Serve.cs
+++ src/Lit.Core/Features/Serve.cs
@@ -1,4 +1,5 @@
 using CommandLine;
+using Lit.Core.Services;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,13 +10,22 @@ namespace Lit.Core.Features
     {
         [Verb("serve")]
         internal class Request : IRequest<Unit> {
-
+            [Option('d', Required = false)]
+            public string Directory { get; set; } = System.Environment.CurrentDirectory;
         }
 
         internal class Handler : IRequestHandler<Request, Unit>
         {
+            private readonly ICommandService _commandService;
+            public Handler(ICommandService commandService)
+            {
+                _commandService = commandService;
+            }
+
             public async Task<Unit> Handle(Request request, CancellationToken cancellationToken)
             {
+                _commandService.Start("npm run serve", request.Directory);
+
                 return new();
             }
         }
